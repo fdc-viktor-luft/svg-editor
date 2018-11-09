a145dd9bfcbec6c1d3f265b1b5fa123a3be2417f
Viktor Luft <viktor.luft@freiheit.com>
2018-11-09 20:24:37 +0100

    Prepared docs for github pages

---
+++ docs/precache-manifest.79d8be62992dea80dc1953da355d9bc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59f6a1459ebd13fc7394",
+    "url": "/svg-editor/static/js/runtime~main.59f6a145.js"
+  },
+  {
+    "revision": "0fb8cfdbde94825ea4d9",
+    "url": "/svg-editor/static/js/main.0fb8cfdb.chunk.js"
+  },
+  {
+    "revision": "b1d963401e099f7db960",
+    "url": "/svg-editor/static/js/1.b1d96340.chunk.js"
+  },
+  {
+    "revision": "0fb8cfdbde94825ea4d9",
+    "url": "/svg-editor/static/css/main.f72380a6.chunk.css"
+  },
+  {
+    "revision": "d618ad846e6d88a605f7b9c7af8633a4",
+    "url": "/svg-editor/index.html"
+  }
+];
